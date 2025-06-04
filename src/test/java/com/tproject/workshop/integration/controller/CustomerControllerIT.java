79152c339258988c057b6a39ed497e57a191f345
OrnelasD-Rogers <ornelas95@hotmail.com>
2025-06-03 23:35:47 -0300

    format CustomerControllerIT

--- src/test/java/com/tproject/workshop/integration/controller/CustomerControllerIT.java
+++ src/test/java/com/tproject/workshop/integration/controller/CustomerControllerIT.java
@@ -45,11 +45,12 @@ public class CustomerControllerIT extends AbstractIntegrationLiveTest {
                 Arguments.of(1, HttpStatus.SC_OK, Map.of(), "get all Customers")
         );
     }
+
     @Order(2)
     @DisplayName("find by name")
     @MethodSource("findCustomersArguments")
     @ParameterizedTest(name = "{displayName} : {0} status {1} body {2} reason {3}")
-    public void findCustomersByName(int index, Integer statusCode, String name, String reason){
+    public void findCustomersByName(int index, Integer statusCode, String name, String reason) {
         Response response = given().spec(SPEC)
                 .when()
                 .get(BASE_PATH + "/find/" + name)
@@ -60,17 +61,19 @@ public class CustomerControllerIT extends AbstractIntegrationLiveTest {
 
         super.validateResponse(index, response);
     }
-    public static Stream<Arguments> findCustomersArguments(){
+
+    public static Stream<Arguments> findCustomersArguments() {
         return Stream.of(
                 Arguments.of(1, HttpStatus.SC_OK, "Alfonso", ""),
-                Arguments.of(2,HttpStatus.SC_OK,"XXX","")
+                Arguments.of(2, HttpStatus.SC_OK, "XXX", "")
         );
     }
+
     @Order(3)
     @DisplayName("find by Id")
     @MethodSource("findCustomersByIdArguments")
     @ParameterizedTest(name = "{displayName} : {0} status {1} body {2} reason {3}")
-    public void findCustomersById(int index, Integer statusCode, int id, String reason){
+    public void findCustomersById(int index, Integer statusCode, int id, String reason) {
         Response response = given().spec(SPEC)
                 .when()
                 .get(BASE_PATH + "/" + id)
@@ -81,17 +84,19 @@ public class CustomerControllerIT extends AbstractIntegrationLiveTest {
 
         super.validateResponse(index, response);
     }
-    public static Stream<Arguments> findCustomersByIdArguments(){
+
+    public static Stream<Arguments> findCustomersByIdArguments() {
         return Stream.of(
                 Arguments.of(1, HttpStatus.SC_OK, 1, ""),
-                Arguments.of(2,HttpStatus.SC_NOT_FOUND,250,"")
+                Arguments.of(2, HttpStatus.SC_NOT_FOUND, 250, "")
         );
     }
+
     @Order(4)
     @DisplayName("Create Customers")
     @MethodSource("createCustomersArguments")
     @ParameterizedTest(name = "{displayName} : {0} status {1} body {2} reason {3}")
-    public void createCustomers(int index, Integer statusCode, Map<String,Object> arguments, String reason){
+    public void createCustomers(int index, Integer statusCode, Map<String, Object> arguments, String reason) {
         Response response = given().spec(SPEC)
                 .when()
                 .contentType("application/json")
@@ -104,10 +109,11 @@ public class CustomerControllerIT extends AbstractIntegrationLiveTest {
 
         super.validateResponseIgnoreAttributes(index, response, List.of("idCustomer", "insertDate"));
     }
-    public static Stream<Arguments> createCustomersArguments(){
+
+    public static Stream<Arguments> createCustomersArguments() {
         return Stream.of(
-                Arguments.of(1, HttpStatus.SC_OK, Map.of("name", "generoso", "cpf", "31781477051", "gender", "m", "mail", "geneder@live.com","phone","4499956752","cellphone","5594941236"), ""),
-                Arguments.of(2, HttpStatus.SC_BAD_REQUEST, Map.of("name", "generoso", "cpf", "317.814.770-40", "gender", "m", "mail", "geneder@live.com","phone","4499956752","cellphone","5594941236"), "wrong cpf number")
-                );
+                Arguments.of(1, HttpStatus.SC_OK, Map.of("name", "generoso", "cpf", "31781477051", "gender", "m", "mail", "geneder@live.com", "phone", "4499956752", "cellphone", "5594941236"), ""),
+                Arguments.of(2, HttpStatus.SC_BAD_REQUEST, Map.of("name", "generoso", "cpf", "317.814.770-40", "gender", "m", "mail", "geneder@live.com", "phone", "4499956752", "cellphone", "5594941236"), "wrong cpf number")
+        );
     }
 }
